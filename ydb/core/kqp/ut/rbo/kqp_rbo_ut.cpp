e8a88aa22438816c5ec917a302aec676e5311da6
Denis Khalikov <deniskhalikov@ydb.tech>
2025-11-21 12:56:02 +0300

    Add tests

--- ydb/core/kqp/ut/rbo/kqp_rbo_ut.cpp
+++ ydb/core/kqp/ut/rbo/kqp_rbo_ut.cpp
@@ -537,7 +537,6 @@ Y_UNIT_TEST_SUITE(KqpRbo) {
         UNIT_ASSERT_C(resultUpsert.IsSuccess(), resultUpsert.GetIssues().ToString());
 
         std::vector<std::string> queries = {
-            /*
             R"(
                 --!syntax_pg
                 SET TablePathPrefix = "/Root/";
@@ -646,17 +645,16 @@ Y_UNIT_TEST_SUITE(KqpRbo) {
                             then 1
                             else 0 end) + 1, sum(t1.a) as r, t1.b + 2 as group_key from t1 group by t1.b + 2 order by r;
 
-            )"
+            )",
             R"(
                 --!syntax_pg
                 SET TablePathPrefix = "/Root/";
                 select distinct t1.b from t1;
             )",
-            */
             R"(
                 --!syntax_pg
                 SET TablePathPrefix = "/Root/";
-                select count(distinct t1.b) from t1 group by t1.a, t1.c order by t1.a;
+                select count(distinct t1.a), t1.b from t1 group by t1.b, t1.c order by t1.b;
             )",
 
         };
@@ -682,14 +680,16 @@ Y_UNIT_TEST_SUITE(KqpRbo) {
             R"([["1";"1"];["1";"3"];["2";"0"];["2";"2"];["2";"4"]])",
             R"([["5";"1"];["7";"2"]])",
             R"([["3";"4";"3"];["3";"6";"4"]])",
+            R"([["2"];["1"]])",
+            R"([["2";"1"];["3";"2"]])"
         };
 
         for (ui32 i = 0; i < queries.size(); ++i) {
             const auto &query = queries[i];
             auto result = session2.ExecuteDataQuery(query, TTxControl::BeginTx().CommitTx()).GetValueSync();
             UNIT_ASSERT_C(result.IsSuccess(), result.GetIssues().ToString());
-            Cout << "OUTPUT_RESULT " << FormatResultSetYson(result.GetResultSet(0)) << Endl;
-            //UNIT_ASSERT_VALUES_EQUAL(FormatResultSetYson(result.GetResultSet(0)), results[i]);
+            //Cout << "OUTPUT_RESULT " << FormatResultSetYson(result.GetResultSet(0)) << Endl;
+            UNIT_ASSERT_VALUES_EQUAL(FormatResultSetYson(result.GetResultSet(0)), results[i]);
         }
     }
 
